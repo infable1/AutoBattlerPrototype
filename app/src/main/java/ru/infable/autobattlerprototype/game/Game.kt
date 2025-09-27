ae6d2a89b51b128285ebf14326ac430e45734036
infable1 <nqsarahos@mail.ru>
2025-09-27 19:03:58 +0300

    Change fun SimulateBattle with use UI

--- app/src/main/java/ru/infable/autobattlerprototype/game/Game.kt
+++ app/src/main/java/ru/infable/autobattlerprototype/game/Game.kt
@@ -53,46 +53,36 @@ object GameLogic {
         player.restoreHealth()
         monster.restoreHealth()
         var turn = 1
-        var currentAttacker: Any = if (player.dexterity > monster.dexterity || player.dexterity == monster.dexterity) player else monster
-        var currentDefender: Any = if (currentAttacker == player) monster else player
+        var currentAttacker = if (player.dexterity > monster.dexterity || player.dexterity == monster.dexterity) player else monster
+        var currentDefender = if (currentAttacker == player) monster else player
+        val log = StringBuilder()
 
         while (player.isAlive() && monster.isAlive()) {
-            val hitChance = Random.nextInt(1, (if (currentAttacker is Character) currentAttacker.dexterity else (currentAttacker as Monster).dexterity) +
-                    (if (currentDefender is Character) currentDefender.dexterity else (currentDefender as Monster).dexterity) + 1)
+            val attackerDex = if (currentAttacker is Character) currentAttacker.dexterity else (currentAttacker as Monster).dexterity
+            val defenderDex = if (currentDefender is Character) currentDefender.dexterity else (currentDefender as Monster).dexterity
+            val hitChance = Random.nextInt(1, attackerDex + defenderDex + 1)
             val targetDex = if (currentDefender is Character) currentDefender.dexterity else (currentDefender as Monster).dexterity
 
-            if (hitChance > targetDex) { // Попадание (по ТЗ: если число <= ловкости цели - промах)
+            if (hitChance > targetDex) { // По ТЗ: промах, если <= ловкости цели
                 val damage = when (currentAttacker) {
-                    is Character -> {
-                        // Если атакующий - Character, цель должна быть Monster (player атакует monster)
-                        if (currentDefender !is Monster) throw IllegalStateException("Ожидался Monster как цель для Character")
-                        currentAttacker.calculateDamage(currentDefender as Monster, turn)
-                    }
-                    is Monster -> {
-                        // Если атакующий - Monster, цель всегда player (Character)
-                        if (currentDefender !is Character) throw IllegalStateException("Ожидался Character как цель для Monster")
-                        currentAttacker.calculateDamage(currentDefender as Character, turn)
-                    }
+                    is Character -> currentAttacker.calculateDamage(currentDefender as Monster, turn)
+                    is Monster -> (currentAttacker as Monster).calculateDamage(player, turn)
                     else -> 0
                 }
                 if (damage > 0) {
-                    when (currentDefender) {
-                        is Character -> currentDefender.takeDamage(damage)
-                        is Monster -> currentDefender.takeDamage(damage)
-                    }
-                    println("${if (currentAttacker is Character) "Игрок" else (currentAttacker as Monster).name} наносит $damage урона. HP цели: ${if (currentDefender is Character) currentDefender.currentHealth else (currentDefender as Monster).currentHealth}")
+                    if (currentDefender is Character) currentDefender.takeDamage(damage) else (currentDefender as Monster).takeDamage(damage)
+                    log.append("${if (currentAttacker is Character) "Игрок" else (currentAttacker as Monster).name} наносит $damage урона. HP: ${if (currentDefender is Character) currentDefender.currentHealth else (currentDefender as Monster).currentHealth}\n")
                 }
             } else {
-                println("Промах!")
+                log.append("Промах!\n")
             }
 
             if (!player.isAlive()) return false
             if (!monster.isAlive()) return true
 
-            // Смена хода
             currentAttacker = currentDefender.also { currentDefender = currentAttacker }
             turn++
         }
-        return false // Не должно дойти сюда
+        return false
     }
 }
